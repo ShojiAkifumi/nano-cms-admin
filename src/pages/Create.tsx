7ef6a9bf1761aa81a5a8846892c0e922d2545eef
aki <kuonnrh48951@outlook.jp>
2023-08-10 17:59:58 +0900

    my first commit

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -4,33 +4,32 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid/Grid";
 import { Editor, DateField, SubmitBtn } from "../components/Editor";
 
-const createPost = () => {};
-
 const Create: React.FC = () => {
-  const [title, setTitle] = useState<string>("");
-  const [content, setContent] = useState<string>("");
-  return (
-    <Dashboard>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={8}>
-          <Paper sx={{ p: 3 }}>
-            <Editor
-              title={title}
-              content={content}
-              setTitle={setTitle}
-              setContent={setContent}
-            />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Paper sx={{ p: 3 }}>
-            <DateField />
-            <SubmitBtn label="公開する" />
-          </Paper>
-        </Grid>
-      </Grid>
-    </Dashboard>
-  );
+    const [title, setTitle] = useState<string>("");
+    const [date, setDate] = useState<string>(new Date().toString());
+    const [content, setContent] = useState<string>("");
+    return (
+        <Dashboard>
+            <Grid container spacing={3}>
+                <Grid item xs={12} md={8}>
+                    <Paper sx={{ p: 3 }}>
+                        <Editor
+                            title={title}
+                            content={content}
+                            setTitle={setTitle}
+                            setContent={setContent}
+                        />
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Paper sx={{ p: 3 }}>
+                        <DateField date={date} setDate={setDate} />
+                        <SubmitBtn label="公開する" />
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Dashboard>
+    );
 };
 
 export default Create;
--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Dashboard from "../components/Dashboard";
 import Paper from "@mui/material/Paper";
